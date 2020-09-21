52adb884dff4b0d24ea84e673ce4d13cfb408e54
Kirill <kupuk132@gmail.com>
2020-09-22 02:17:42 +0300

    write cocktailSort

---
+++ cocktailSort.js
@@ -0,0 +1,23 @@
+function cocktailSort(arr) {
+  let left = 0;
+  let right = arr.length-1;
+  while(left < right) {
+    for(let i = left; i < right; i++) {
+      if(arr[i] > arr[i+1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = tmp;
+      }
+    }
+    right--;
+    for(let i = right; i > left; i--) {
+      if(arr[i] < arr[i-1]) {
+        let tmp = arr[i];
+        arr[i] = arr[i-1];
+        arr[i-1] = tmp;
+      }
+    }
+    left++;
+  }
+  return arr;
+}

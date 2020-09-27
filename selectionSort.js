3ff66d9230d1db518fea9b8e70872a052cde1878
Kirill <kupuk132@gmail.com>
2020-09-27 20:56:13 +0300

    write selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+  return arr;
+}
